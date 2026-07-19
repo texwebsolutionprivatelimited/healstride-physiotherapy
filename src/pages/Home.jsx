ab71937d393a8f045f11ca600113faf1d337458a
Prathyusha Morthala <prathyusha290105@gmail.com>
2026-07-19 14:51:52 +0530

    Improve blog section layout and article page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ import BlogSection from "../components/home/BlogSection";
 import { blogs } from "../data/blogs";
 import TreatmentSlider from "../components/home/TreatmentSlider";
 import WhatsAppFloat from "../components/common/WhatsAppFloat";
-import BlogSection from "../components/home/BlogSection";
+
 
 
 const Home = () => {
@@ -25,8 +25,12 @@ const Home = () => {
       <Conditions /> 
       <GalleryPreview /> 
       <Specialists /> 
+      <BlogSection
+  blogsToShow={blogs.slice(0, 3)}
+  showButton={false}
+  showViewAllButton={true}
+/>
       <Testimonials />
-      <BlogSection />
       <WhatsAppFloat />
        
     </>
